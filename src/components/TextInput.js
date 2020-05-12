b1506a9b3e9f61db0ddaf81a94f1ee758f73c6d5
Carlos Corti <carlosacorti@hotmail.com>
2020-05-12 14:41:20 +0200

    adding PropTypes and error message to TextInput

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,12 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const TextInput = (props) => {
+  const errorClass = props.error.length > 0 ? "has-error" : "";
   const lowerName = props.name.toLowerCase();
+
   return (
-    <div className="form-group">
-      <label htmlFor={lowerName}>{`${props.name
-        .charAt(0)
-        .toUpperCase()}${props.name.slice(1).toLowerCase()}`}</label>
+    <div className={`form-group ${errorClass}`}>
+      <label htmlFor={lowerName}>
+        {`${props.name.charAt(0).toUpperCase()}${props.name
+          .slice(1)
+          .toLowerCase()}`}
+      </label>
       <div className="field">
         <input
           id={lowerName}
@@ -17,8 +22,21 @@ const TextInput = (props) => {
           value={props.val}
         />
       </div>
+      {props.error && <div className="alert alert-danger">{props.error}</div>}
     </div>
   );
+};
+
+TextInput.propTypes = {
+  inputHandler: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  val: PropTypes.string,
+  error: PropTypes.string,
+};
+
+TextInput.defaultProps = {
+  val: "no val pased, recomend pass some React state props",
+  error: "",
 };
 
 export default TextInput;
